cfd599c1708f992a421e2ea62a41dacc780e7b2f
Bhavan-Prakash <bhavanofficial2004@gmail.com>
2026-07-06 21:25:05 +0530

    06 july

--- Air-Board/main.py
+++ Air-Board/main.py
@@ -26,6 +26,14 @@ landmarker = HandLandmarker.create_from_options(options)
 
 cap = cv2.VideoCapture(0)
 
+prev_x = None
+prev_y = None
+
+res, frame = cap.read()
+height, width, _ = frame.shape
+
+canvas = np.zeros((height, width, 3), dtype=np.uint8)
+
 while(True):
     res, frame = cap.read()
 
@@ -35,6 +43,63 @@ while(True):
     frame = cv2.flip(frame, 1)
 
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    #region clear button code begins
+    # Clear button coordinates 
+    button_width = 200
+    button_height = 70
+
+    # Left-middle position
+    x1_clear_button = 20
+    y1_clear_button = (height - button_height) // 2
+
+    x2_clear_button = x1_clear_button + button_width
+    y2_clear_button = y1_clear_button + button_height
+
+    # Draw rectangle
+    cv2.rectangle(frame, (x1_clear_button, y1_clear_button), (x2_clear_button, y2_clear_button), (0, 0, 255), 2)
+
+    # Write CLEAR inside it
+    text = "CLEAR"
+
+    (text_width, text_height), _ = cv2.getTextSize(
+        text,
+        cv2.FONT_HERSHEY_SIMPLEX,
+        0.8,
+        2
+    )
+
+    text_x = x1_clear_button + (button_width - text_width) // 2
+    text_y = y1_clear_button + (button_height + text_height) // 2
+
+    cv2.putText(
+        frame,
+        text,
+        (text_x, text_y),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 0, 255),
+        5
+    )
+
+    #endregion
+
+
+    #region draw button code begins
+
+    margin = 20
+
+    x1_top = x1_clear_button
+    y1_top = y1_clear_button - button_height - margin
+
+    x2_top = x1_top + button_width
+    y2_top = y1_top + button_height
+
+    cv2.rectangle(frame, (x1_top, y1_top), (x2_top, y2_top), (255, 0, 0), 2)
+    cv2.putText(frame, "SAVE", (x1_top + 45, y1_top + 45),
+    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
+
+    #endregion
 
     height, width, _ = frame.shape
 
@@ -50,9 +115,25 @@ while(True):
             for i, coordinate in enumerate(hands):
                 px = int(coordinate.x * width)
                 py = int(coordinate.y * height)
-                cv2.circle(frame, (px,py), 5, (0,255,0), -1)
+                if i==8:
+                    cv2.circle(frame, (px,py), 5, (0,255,0), -1)
 
-    cv2.imshow("testing", frame)
+                    if x1_clear_button < px < x2_clear_button and y1_clear_button < py < y2_clear_button:
+                        canvas[:] = 0
+
+                    if prev_x is not None and prev_y is not None:
+                        cv2.line(canvas, (prev_x,prev_y), (px,py), (0,255,0), 5)
+                    prev_x = px
+                    prev_y = py
+    else:
+        # Finger lost -> stop connecting old point to new point
+        prev_x = None
+        prev_y = None
+
+    # Overlay drawing on camera feed
+    output = cv2.add(frame, canvas)
+
+    cv2.imshow("testing", output)
 
     # press esc key to close
     if cv2.waitKey(1) == 27:
